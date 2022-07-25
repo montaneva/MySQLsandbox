3fe867d01aa6201602c8ce47996622edcda5caf0
Eva Montané <eva@evamontane.com>
2022-07-25 21:55:18 +0200

    Update myquery_round.sql

--- round_2digits/myquery_round.sql
+++ round_2digits/myquery_round.sql
@@ -45,3 +45,18 @@ GROUP BY gender;
 # | W      |     6 |   85.71 |
 # +--------+-------+---------+
 
+
+CREATE VIEW view_gender as
+SELECT gender as 'Gender',
+COUNT(gender) as Count,
+ROUND((count(gender) / (SELECT COUNT(gender) FROM mytbl_gender)*100),2) as Percent FROM mytbl_gender
+GROUP BY Gender ORDER BY Count ASC;
+
+SELECT * FROM view_gender;
+# +--------+-------+---------+
+# | Gender | Count | Percent |
+# +--------+-------+---------+
+# | M      |     1 |   14.29 |
+# | W      |     6 |   85.71 |
+# +--------+-------+---------+
+
